8119df0398fcea15d823db0256395cfe715570f8
bhholuuu <dudhatrabhavya880@gmail.com>
2025-01-02 13:20:25 +0530

    header.php page created

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
+        integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div class="header">
+        <div class="first">
+            <div class="logo">
+                <a href="index.php"><img src="assets/img/logo.png" alt="logo"></a>
+            </div>
+        </div>
+        <div class="second">
+            <div class="search">
+                <input type="text" placeholder="Search..." size="50">
+                <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+        </div>
+        <div class="third">
+            <div class="options">
+                <a href="#" title="Category"><i class="fa-solid fa-folder"></i></a>
+                <a href="#" title="Course"><i class="fa-solid fa-book"></i></a>
+                <a href="#" title="My Batch"><i class="fa-solid fa-bookmark"></i></a>
+                <a href="#" title="Profile"><img src="assets/img/bhavya.jpg" alt=""></a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
